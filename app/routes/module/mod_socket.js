a6e498231c87d358be547b0f062d3319df58c9bf
kunou <t-kunou@esm.co.jp>
2019-03-03 12:38:40 +0900

    一旦動くようにした

---
+++ app/routes/module/mod_socket.js
@@ -0,0 +1,35 @@
+var http = require('http');
+//サーバインスタンス作成
+var server = http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.end('server connected');
+});
+var io = require('socket.io').listen(server);
+
+server.listen(8888);//8888番ポートで起動
+console.log('Wait for connect');
+
+var tunkSocket = null;
+var mobileSocket = null;
+
+//接続確立時の処理
+io.sockets.on('connection', function (socket) {
+  // この中でデータのやり取りを行う
+  console.log('connected');
+
+  socket.on('registration', function(name) {
+    console.log('Registration from ' + name);
+    if (name === 'tunk') {
+      tunkSocket = socket;
+    } else if (name === 'mobile') {
+      mobileSocket = socket;
+    }
+  });
+
+  socket.on('move', function(d){
+    console.log(d);
+    if (tunkSocket) {
+      tunkSocket.emit('move', d)
+    }
+  });
+});
